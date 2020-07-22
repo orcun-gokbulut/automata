e2f1def42ceef9cfd2d44489337cfffbaecb45f9
orcun.gokbulut <orcun.gokbulut@gmail.com>
2020-07-23 01:10:06 +0300

    ADD: Automata workspace added. ADD: Save and open functionality have been added to relay configurator. MOD: Many bug fixes and internal improvements have been made on relay configurator.

--- automata-configurator/src/ARelayConfiguratorDialog.cpp
+++ automata-configurator/src/ARelayConfiguratorDialog.cpp
@@ -4,26 +4,24 @@
 #include <QRegExp>
 #include <QUdpSocket>
 #include <QtEndian>
+#include <QFileDialog>
+
 #include "ui_ARelayConfiguratorDialog.h"
 
+const uint32_t ConfigurationHeader = 0x5fe08e26;
 struct Configuration
 {
-    bool configured;
+    uint32_t header;
     uint32_t UUID;
+    char wifiSSID[64];
+    char wifiPassword[64];
     char hostname[32];
-    bool powerOnState;      
     bool DHCP;
     uint32_t IP;
     uint32_t subnet;
-    uint32_t gateway;  
+    uint32_t gateway;
     uint16_t httpPort;
-};
-
-const uint32_t ConfigurationBroadcastPacketHeader = 0xb713e302;
-struct ConfigurationBroadcastPacket
-{
-    uint32_t header;
-    Configuration configuration;
+    bool powerOnState;
 };
 
 const uint32_t ConfigurationBroadcastReplyPacketHeader = 0x53f59729;
@@ -32,40 +30,161 @@ struct ConfigurationBroadcastReplyPacket
     uint32_t header;
 };
 
-static ConfigurationBroadcastPacket packet;
+QString ToStringIP(uint32_t ip)
+{
+    return QString("%1.%2.%3.%4")
+        .arg(((quint8*)&ip)[3])
+        .arg(((quint8*)&ip)[2])
+        .arg(((quint8*)&ip)[1])
+        .arg(((quint8*)&ip)[0]);
+}
+
+void ARelayConfiguratorDialog::loadForm(Configuration* configuration)
+{
+    form->spnUUID->setValue(configuration->UUID);
+    form->txtHostname->setText(configuration->hostname);
+    form->txtWifiSSID->setText(configuration->wifiSSID);
+    form->txtWifiPassword->setText(configuration->wifiPassword);
+    form->chkDHCP->setChecked(configuration->DHCP);
+    form->txtIP->setText(ToStringIP(configuration->IP));
+    form->txtSubnet->setText(ToStringIP(configuration->subnet));
+    form->txtGateway->setText(ToStringIP(configuration->gateway));
+    form->spnHttpPort->setValue(configuration->httpPort);
+    form->chkPowerOn->setChecked(configuration->powerOnState);
+}
+
+void ARelayConfiguratorDialog::saveForm(Configuration* configuration)
+{
+    memset(configuration, 0, sizeof(Configuration));
+
+    configuration->header = ConfigurationHeader;
+    configuration->UUID = form->spnUUID->value();
+
+    QByteArray hostname = form->txtHostname->text().toLocal8Bit();
+    memcpy(configuration->hostname, hostname.data(), hostname.size());
+
+    QByteArray wifiSSID = form->txtWifiSSID->text().toLocal8Bit();
+    memcpy(configuration->wifiSSID, wifiSSID.data(), wifiSSID.size());
+
+    QByteArray wifiPassowrd = form->txtWifiPassword->text().toLocal8Bit();
+    memcpy(configuration->wifiPassword, wifiPassowrd.data(), wifiPassowrd.size());
+
+    configuration->DHCP = form->chkDHCP->isChecked();
+    configuration->IP = QHostAddress(form->txtIP->text()).toIPv4Address();
+    configuration->subnet = QHostAddress(form->txtSubnet->text()).toIPv4Address();
+    configuration->gateway = QHostAddress(form->txtGateway->text()).toIPv4Address();
+
+    configuration->httpPort = form->spnHttpPort->value();
+    configuration->powerOnState = form->chkPowerOn->isChecked();
+
+    validate();
+}
+
+void ARelayConfiguratorDialog::mnuNew_clicked()
+{
+    Configuration configuration;
+    memset(&configuration, 0, sizeof(Configuration));
+    configuration.UUID = qrand() % 999999999;
+    configuration.DHCP = true;
+    configuration.httpPort = 80;
+    strcpy(configuration.hostname, "UnnamedRelay");
+    configuration.IP = QHostAddress("192.168.1.61").toIPv4Address();
+    configuration.subnet = QHostAddress("255.255.255.0").toIPv4Address();
+    configuration.gateway = QHostAddress("192.168.1.1").toIPv4Address();
+    loadForm(&configuration);
+}
+
+void ARelayConfiguratorDialog::mnuOpen_clicked()
+{
+    QString path = QFileDialog::getOpenFileName(this, "Open Configuration File", QString(), "Relay Configuration File (*.rconf);;All Files (*.*)");
+    if (path.isEmpty())
+        return;
+
+    QFile file(path);
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        QMessageBox::critical(this, "File Read Error", "Cannot open file for reading.", QMessageBox::Ok);
+        return;
+    }
+
+    Configuration configuration;
+    qint64 size = file.read((char*)&configuration, sizeof(Configuration));
+    if (size != sizeof(Configuration) || configuration.header != ConfigurationHeader)
+    {
+        QMessageBox::critical(this, "File Read Error", "File is not Relay Configuration file.", QMessageBox::Ok);
+        return;
+    }
+    file.close();
+
+    filename = path;
+
+    loadForm(&configuration);
+}
+
+void ARelayConfiguratorDialog::mnuSave_clicked()
+{
+    if (filename.isEmpty())
+        mnuSaveAs_clicked();
+
+    QFile file(filename);
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        QMessageBox::critical(this, "File Write Error", "Cannot open file for writing.", QMessageBox::Ok);
+        return;
+    }
+
+    Configuration configuration;
+    saveForm(&configuration);
+
+    qint64 size = file.write((char*)&configuration, sizeof(Configuration));
+    if (size != sizeof(Configuration))
+    {
+        QMessageBox::critical(this, "File Read Error", "Cannot write to the file.", QMessageBox::Ok);
+        return;
+    }
+    file.close();
+}
+
+void ARelayConfiguratorDialog::mnuSaveAs_clicked()
+{
+    QString path = QFileDialog::getSaveFileName(this, "Save Configuration File", QString(), "Relay Configuration File (*.rconf);;All Files (*.*)");
+    if (path.isEmpty())
+        return;
+
+    QFile file(path);
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        QMessageBox::critical(this, "File Write Error", "Cannot open file for writing.", QMessageBox::Ok);
+        return;
+    }
+
+    Configuration configuration;
+    saveForm(&configuration);
+
+    qint64 size = file.write((char*)&configuration, sizeof(Configuration));
+    if (size != sizeof(Configuration))
+    {
+        QMessageBox::critical(this, "File Read Error", "Cannot write to the file.", QMessageBox::Ok);
+        return;
+    }
+    file.close();
+
+    filename = path;
+}
+
+void ARelayConfiguratorDialog::mnuExit_clicked()
+{
+    close();
+}
 
 void ARelayConfiguratorDialog::btnStartBroadcasting_clicked()
 {
     if (!broadcastTimer->isActive())
     {
-        ConfigurationBroadcastPacket packet;
-        packet.header = ConfigurationBroadcastPacketHeader;
-        
-        packet.configuration.UUID = form->spnUUID->value();
-
-        QByteArray hostname = form->txtHostname->text().toUtf8();
-        memcpy(packet.configuration.hostname, hostname.data(), hostname.size());
-        
-        packet.configuration.DHCP = form->chkDHCP->isChecked();
-        if (!packet.configuration.DHCP)
-        {
-            packet.configuration.IP = QHostAddress(form->txtIP->text()).toIPv4Address();
-            packet.configuration.subnet = QHostAddress(form->txtSubnet->text()).toIPv4Address();
-            packet.configuration.gateway = QHostAddress(form->txtGateway->text()).toIPv4Address();
-        }
-        else
-        {
-            packet.configuration.IP = 0x00000000;
-            packet.configuration.subnet = 0x00000000;
-            packet.configuration.gateway = 0x00000000;
-        }
-
-        packet.configuration.powerOnState = form->chkPowerOn->isChecked();
-        packet.configuration.httpPort = form->spnHttpPort->value();
 
         if (!socket.bind(1089))
         {
-            QMessageBox::critical(this, "Cannot bind socket to port 1089.", "Error Occured", QMessageBox::Ok);
+            QMessageBox::critical(this, "Network Error", "Cannot bind socket to port 1089.", QMessageBox::Ok);
             return;
         }
 
@@ -83,6 +202,8 @@ void ARelayConfiguratorDialog::btnStartBroadcasting_clicked()
 
     form->spnUUID->setEnabled(!broadcastTimer->isActive());
     form->txtHostname->setEnabled(!broadcastTimer->isActive());
+    form->txtWifiSSID->setEnabled(!broadcastTimer->isActive());
+    form->txtWifiPassword->setEnabled(!broadcastTimer->isActive());
     form->chkDHCP->setEnabled(!broadcastTimer->isActive());
     form->txtIP->setEnabled(!broadcastTimer->isActive() && !form->chkDHCP->isChecked());
     form->txtSubnet->setEnabled(!broadcastTimer->isActive() && !form->chkDHCP->isChecked());
@@ -95,26 +216,28 @@ void ARelayConfiguratorDialog::broadcast()
 {
     form->lblBroadcast->setVisible(!form->lblBroadcast->isVisible());
 
-    socket.writeDatagram((const char*)&packet, sizeof(ConfigurationBroadcastPacket), QHostAddress::Broadcast, 1089);
-    
-    ConfigurationBroadcastReplyPacket packet;
+    Configuration packet;
+    loadForm(&packet);
+    socket.writeDatagram((const char*)&packet, sizeof(Configuration), QHostAddress::Broadcast, 1089);
+
+    ConfigurationBroadcastReplyPacket replyPacket;
     QHostAddress relayIP;
-    
+
     while (true)
     {
         if (!socket.hasPendingDatagrams())
             break;
 
-        if (socket.readDatagram((char*)&packet, sizeof(ConfigurationBroadcastReplyPacket), &relayIP) != sizeof(ConfigurationBroadcastReplyPacket))
+        if (socket.readDatagram((char*)&replyPacket, sizeof(ConfigurationBroadcastReplyPacket), &relayIP) != sizeof(ConfigurationBroadcastReplyPacket))
             continue;
 
-        if (packet.header != ConfigurationBroadcastReplyPacketHeader)
+        if (replyPacket.header != ConfigurationBroadcastReplyPacketHeader)
             continue;
 
-        QMessageBox::information(this, 
-            QString("Confirmation packed received from a relay with IP {1}. Stopping broadcasting.").arg(relayIP.toString()), 
-            "Confirmation received", QMessageBox::Ok);
-        
+        QMessageBox::information(this, "Confirmation Received",
+            QString("Confirmation packed received from a relay with IP %1. Stopping broadcasting.").arg(relayIP.toString()),
+            QMessageBox::Ok);
+
         btnStartBroadcasting_clicked();
     }
 }
@@ -133,7 +256,7 @@ bool ARelayConfiguratorDialog::validate()
     {
         form->txtHostname->setStyleSheet("");
     }
-    
+
     QRegExp regexIP(
         "\\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
         "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
@@ -178,7 +301,7 @@ bool ARelayConfiguratorDialog::validate()
     }
 
     form->btnStartBroadcasting->setEnabled(validated);
-    
+
     return validated;
 }
 
@@ -188,10 +311,11 @@ ARelayConfiguratorDialog::ARelayConfiguratorDialog()
     form->setupUi(this);
 
     form->lblBroadcast->setVisible(false);
-    form->spnUUID->setValue(qrand() % 999999999);
-    
+
     broadcastTimer = new QTimer(this);
     broadcastTimer->setInterval(500);
+
+    mnuNew_clicked();
 
     QObject::connect(this->form->btnStartBroadcasting, &QAbstractButton::clicked, this, &ARelayConfiguratorDialog::btnStartBroadcasting_clicked);
     QObject::connect(this->form->txtIP, &QLineEdit::textChanged, this, &ARelayConfiguratorDialog::validate);
@@ -199,6 +323,11 @@ ARelayConfiguratorDialog::ARelayConfiguratorDialog()
     QObject::connect(this->form->txtGateway, &QLineEdit::textChanged, this, &ARelayConfiguratorDialog::validate);
     QObject::connect(this->form->chkDHCP, &QCheckBox::stateChanged, this, &ARelayConfiguratorDialog::validate);
     QObject::connect(this->broadcastTimer, &QTimer::timeout, this, &ARelayConfiguratorDialog::broadcast);
+    QObject::connect(this->form->mnuNew, &QAction::triggered, this, &ARelayConfiguratorDialog::mnuNew_clicked);
+    QObject::connect(this->form->mnuOpen, &QAction::triggered, this, &ARelayConfiguratorDialog::mnuOpen_clicked);
+    QObject::connect(this->form->mnuSave, &QAction::triggered, this, &ARelayConfiguratorDialog::mnuSave_clicked);
+    QObject::connect(this->form->mnuSaveAs, &QAction::triggered, this, &ARelayConfiguratorDialog::mnuSaveAs_clicked);
+    QObject::connect(this->form->mnuExit, &QAction::triggered, this, &ARelayConfiguratorDialog::mnuExit_clicked);
 
     validate();
 }
--- automata-configurator/src/ARelayConfiguratorDialog.h
+++ automata-configurator/src/ARelayConfiguratorDialog.h
@@ -1,18 +1,28 @@
-#include <QDialog>
+#include <QMainWindow>
 #include <QTimer>
 #include <QUdpSocket>
 
 class Ui_ARelayConfiguratorDialog;
+class Configuration;
 
-class ARelayConfiguratorDialog : public QDialog
+class ARelayConfiguratorDialog : public QMainWindow
 {
     private:
         Ui_ARelayConfiguratorDialog* form;
         QTimer* broadcastTimer;
         QUdpSocket socket;
+        QString filename;
 
-    
+        void saveForm(Configuration* configuration);
+        void loadForm(Configuration* configuration);
+
+
     private slots:
+        void mnuNew_clicked();
+        void mnuOpen_clicked();
+        void mnuSave_clicked();
+        void mnuSaveAs_clicked();
+        void mnuExit_clicked();
         void btnStartBroadcasting_clicked();
         void broadcast();
         bool validate();
